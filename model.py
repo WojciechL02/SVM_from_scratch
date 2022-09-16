a2852198e954714e0b90d24fbc8f9c9456cbbaf1
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2022-09-16 13:43:33 +0200

    fit method

--- model.py
+++ model.py
@@ -3,20 +3,31 @@ import numpy as np
 
 class SVM:
     def __init__(self, learning_rate, lambda_param, n_iters) -> None:
-        self.learning_rate = learning_rate
+        self.lr = learning_rate
         self.lambda_param = lambda_param
         self.n_iters = n_iters
         self.weights = None
         self.bias = None
 
-    def fit(self):
-        pass
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.b = 0
+        y_ = np.where(y >= 0, 1, -1)
+
+        for _ in range(self.n_iters):
+            for idx, x_i in enumerate(X):
+                condition = y_[idx] * (np.dot(self.weights, x_i) - self.bias) >= 1
+                if condition:
+                    dw = 2 * self.lambda_param * self.weights
+                    self.weights -= self.lr * dw
+                else:
+                    dw = 2 * self.lambda_param * self.weights - y_[idx] * x_i
+                    db = y_[idx]
+                    self.weights -= self.lr * dw
+                    self.bias -= self.lr * db
 
     def predict(self, X):
         output = np.dot(self.weights, X) - self.bias
         return np.sign(output)
-
-
-
-
 
