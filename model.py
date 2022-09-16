2e5545eff5aea6230f3628f38a9a6890125f9564
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2022-09-16 13:27:52 +0200

    predict method

--- model.py
+++ model.py
@@ -1,6 +1,20 @@
 import numpy as np
 
 
+class SVM:
+    def __init__(self, learning_rate, lambda_param, n_iters) -> None:
+        self.learning_rate = learning_rate
+        self.lambda_param = lambda_param
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+
+    def fit(self):
+        pass
+
+    def predict(self, X):
+        output = np.dot(self.weights, X) - self.bias
+        return np.sign(output)
 
 
 
